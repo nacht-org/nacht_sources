2555302a3067a489e7c2d8ef7c190125bfad7466
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-12-27 12:20:29 +0500

    Improved documentation

--- lib/src/models/meta.dart
+++ lib/src/models/meta.dart
@@ -3,13 +3,20 @@ import './work_type.dart';
 
 /// Defines the optional features of a crawler
 enum Feature {
+  /// Ability to search for novels
   search,
+
+  /// Ability to login into the source to
+  /// otherwise forbidden content
   login,
 }
 
 /// Defines the reading direction of crawler
 enum ReadingDirection {
+  /// Left to right
   ltr,
+
+  /// Right to left
   rtl,
 
   /// Identifies that a source [Meta] has mixed reading direction
@@ -31,12 +38,40 @@ class DateHolder {
   DateTime datetime() => DateTime(year, month, day);
 }
 
+/// Defines the metadata of a crawler
 class Meta {
+  /// The name of the source. Usually equivalent to the
+  /// name of the crawler class
   final String name;
+
+  /// The languages of the novels offered by the source
+  ///
+  /// Use abbreviations of languages. ex: 'en' for English
+  /// and 'de' for German
+  ///
+  /// Use comma-separated values for multiple specific
+  /// values. ex: 'en,de'
+  ///
+  /// Use 'mixed' if source offers novels in
+  /// multiple languages
   final String lang;
+
+  /// The last time the source was updated
   final DateHolder updated;
+
+  /// The base urls / host urls of the target site
+  ///
+  /// Must include the scheme (http:,https:)
+  ///
+  /// ex: 'https://www.webnovel.com/', 'https://www.scribblehub.com/'
   final Set<String> baseUrls;
+
+  /// Optional features supported by the crawler
+  ///
+  /// ex: search, login
   final Set<Feature> features;
+
+  /// Identifies the support for this crawler
   final Support support;
 
   /// Identifies the type of work provided by this source
