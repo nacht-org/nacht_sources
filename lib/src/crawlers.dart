71f08396e2ad188acd325406995d353508635bbb
Mensch <mensch272@users.noreply.github.com>
2022-04-10 05:32:21 +0500

    Added novelpub to crawlers list.

--- lib/src/crawlers.dart
+++ lib/src/crawlers.dart
@@ -2,11 +2,13 @@
 
 import "package:chapturn_sources/src/utils.dart";
 import "package:chapturn_sources/src/impls/fanfiction.dart";
-import "package:chapturn_sources/src/impls/scribblehub.dart";
 import "package:chapturn_sources/src/impls/royalroad.dart";
+import "package:chapturn_sources/src/impls/scribblehub.dart";
+import "package:chapturn_sources/src/impls/novelpub.dart";
 
 const crawlers = [
 CrawlerFactory(FanFiction.constMeta, FanFiction.make, FanFiction.makeWith),
-CrawlerFactory(ScribbleHub.constMeta, ScribbleHub.make, ScribbleHub.makeWith),
 CrawlerFactory(RoyalRoad.constMeta, RoyalRoad.make, RoyalRoad.makeWith),
+CrawlerFactory(ScribbleHub.constMeta, ScribbleHub.make, ScribbleHub.makeWith),
+CrawlerFactory(NovelPub.constMeta, NovelPub.make, NovelPub.makeWith),
 ];
