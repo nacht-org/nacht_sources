b8172a2c7dd1ee8afdab347041c20857abc5877f
Mensch <mensch272@users.noreply.github.com>
2022-04-19 06:25:58 +0500

    Test to check if crawler features and impls match.

--- test/generated/crawlers_test.dart
+++ test/generated/crawlers_test.dart
@@ -1,11 +1,38 @@
 import 'package:chapturn_sources/chapturn_sources.dart';
+import 'package:dio/dio.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('all crawlers should have atleast one base url', () {
+  test('crawlers should have atleast one base url', () {
     for (final crawlerFactory in crawlers) {
       final baseUrls = crawlerFactory.meta().baseUrls;
       expect(baseUrls.length, greaterThanOrEqualTo(1));
+    }
+  });
+
+  test('crawlers should identify features implemented', () {
+    final dio = Dio();
+    for (final crawlerFactory in crawlers) {
+      final meta = crawlerFactory.meta();
+      final crawler = crawlerFactory.createWith(dio);
+
+      expect(
+        crawler is NovelSearch,
+        meta.features.contains(Feature.search),
+        reason: "'search' feature not synced in '${meta.name}'",
+      );
+
+      expect(
+        crawler is NovelLogin,
+        meta.features.contains(Feature.login),
+        reason: "'login' feature not synced in '${meta.name}'",
+      );
+
+      expect(
+        crawler is NovelPopular,
+        meta.features.contains(Feature.popular),
+        reason: "'popular' feature not synced in '${meta.name}'",
+      );
     }
   });
 }
