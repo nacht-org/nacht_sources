22fc9abc321eeef18fed9d1d31768a2b9918bd40
Mensch <mensch272@users.noreply.github.com>
2022-06-16 19:57:14 +0500

    fix(tests): removed feature check test.
    The test does not work now since the parser is now unified.

--- test/generated/crawlers_test.dart
+++ test/generated/crawlers_test.dart
@@ -1,5 +1,4 @@
 import 'package:nacht_sources/nacht_sources.dart';
-import 'package:dio/dio.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -7,32 +6,6 @@ void main() {
     for (final crawlerFactory in crawlers) {
       final baseUrls = crawlerFactory.meta().baseUrls;
       expect(baseUrls.length, greaterThanOrEqualTo(1));
-    }
-  });
-
-  test('crawlers should identify features implemented', () {
-    final dio = Dio();
-    for (final crawlerFactory in crawlers) {
-      final meta = crawlerFactory.meta();
-      final crawler = crawlerFactory.custom(dio);
-
-      expect(
-        crawler is ParseSearch,
-        meta.features.contains(Feature.search),
-        reason: "'search' feature not synced in '${meta.name}'",
-      );
-
-      expect(
-        crawler is ParseLogin,
-        meta.features.contains(Feature.login),
-        reason: "'login' feature not synced in '${meta.name}'",
-      );
-
-      expect(
-        crawler is ParsePopular,
-        meta.features.contains(Feature.popular),
-        reason: "'popular' feature not synced in '${meta.name}'",
-      );
     }
   });
 }
